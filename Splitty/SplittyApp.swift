846608c4347131ea702ac34ca85d153e0b6b9361
Lukasz Bigos <luki.bigos@gmail.com>
2026-02-19 15:07:22 +0100

    Initial commit

---
+++ Splitty/SplittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplittyApp.swift
+//  Splitty
+//
+//  Created by Luki on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SplittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
